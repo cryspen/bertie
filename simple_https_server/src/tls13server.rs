e51fff418bfe25a5728670c4839644fee883d878
Damian Poddebniak <poddebniak@mailbox.org>
2022-09-22 16:55:01 +0200

    Improve `main` of simple_https_server.
    * Cleanup
    * Add tracing
    * Handle errors better

--- simple_https_server/src/tls13server.rs
+++ simple_https_server/src/tls13server.rs
@@ -2,22 +2,33 @@
 //! It waits for a connection at port 443, receives an HTTP "GET /", and prints a constant string
 //! WARNING: This code is not in hacspec since it need to use TCP etc.
 
+use anyhow::Context;
+use std::str::FromStr;
 use std::{env, net::TcpListener, time::Duration};
+use tracing_subscriber;
 
 use simple_https_server::tls13server;
 
-pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let host = if args.len() <= 1 {
-        "localhost"
-    } else {
-        &args[1]
+pub fn main() -> anyhow::Result<()> {
+    // Setup tracing.
+    tracing_subscriber::fmt::init();
+
+    // Obtain host and port from arguments.
+    let (host, port) = {
+        let mut args = env::args();
+
+        let _ = args.next().context("Unexpected parameter environment.")?;
+
+        let host = args.next().unwrap_or("localhost".to_string());
+        let port = args.next().unwrap_or("443".to_string());
+
+        (
+            host,
+            u16::from_str(&port).context("Failed to parse port number.")?,
+        )
     };
-    let port = if args.len() <= 2 { "443" } else { &args[2] };
 
-    let addr = ["127.0.0.1", port].join(":");
-
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind((host.as_str(), port)).unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
@@ -27,7 +38,7 @@ pub fn main() {
             .set_read_timeout(Some(d))
             .expect("set_read_timeout call failed");
         println!("New connection established!");
-        match tls13server(&mut stream, host) {
+        match tls13server(&mut stream, &host) {
             Ok(()) => {
                 println!("Connection to {} succeeded\n", host);
             }
@@ -36,4 +47,6 @@ pub fn main() {
             }
         }
     }
+
+    Ok(())
 }
