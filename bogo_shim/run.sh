2477e5c38b718af4f3ff77893d1a13bacfb4b767
Franziskus Kiefer <franziskuskiefer@gmail.com>
2022-11-22 10:13:05 +0000

    don't use evercrypt for bogo

--- bogo_shim/run.sh
+++ bogo_shim/run.sh
@@ -14,7 +14,7 @@ fi
 cwd=$(cd $(dirname $0); pwd -P)
 
 # Build Bertie bogo shim
-cargo build -p bogo_shim --no-default-features --features evercrypt
+cargo build -p bogo_shim --no-default-features
 
 # Run bogo runner
 (cd ${BORINGSSL_ROOT}/ssl/test/runner && \
