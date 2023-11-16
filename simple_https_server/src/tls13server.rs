45c108325d987494995f58539b099e00ee954ae9
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-11-16 21:56:24 +0100

    Allow to select ciphersuite and signing algorithm for simple server

--- simple_https_server/src/tls13server.rs
+++ simple_https_server/src/tls13server.rs
@@ -2,30 +2,62 @@
 //! It waits for a connection at port 443, receives an HTTP "GET /", and prints a constant string
 //! WARNING: This code is not in hacspec since it need to use TCP etc.
 
-use anyhow::Context;
-use std::str::FromStr;
-use std::{env, net::TcpListener, time::Duration};
+use std::{net::TcpListener, time::Duration};
 
 use simple_https_server::tls13server;
+
+mod flags {
+    use std::path::PathBuf;
+
+    xflags::xflags! {
+        cmd listen {
+            /// The hostname, defaults to "localhost"
+            optional --host host: String
+            /// Port to listen on, defaults to port 443
+            optional --port port: u16
+            /// Algorithms to attempt to accept from a client.
+
+            /// Can be one of the following strings:
+            ///
+            ///   * SHA256_Chacha20Poly1305_RsaPssRsaSha256_X25519
+            ///   * SHA256_Chacha20Poly1305_EcdsaSecp256r1Sha256_X25519
+            ///   * SHA256_Chacha20Poly1305_EcdsaSecp256r1Sha256_P256
+            ///   * SHA256_Chacha20Poly1305_RsaPssRsaSha256_P256
+            ///   * SHA256_Aes128Gcm_EcdsaSecp256r1Sha256_P256
+            ///   * SHA256_Aes128Gcm_EcdsaSecp256r1Sha256_X25519
+            ///   * SHA256_Aes128Gcm_RsaPssRsaSha256_P256
+            ///   * SHA256_Aes128Gcm_RsaPssRsaSha256_X25519
+            ///   * SHA384_Aes256Gcm_EcdsaSecp256r1Sha256_P256
+            ///   * SHA384_Aes256Gcm_EcdsaSecp256r1Sha256_X25519
+            ///   * SHA384_Aes256Gcm_RsaPssRsaSha256_P256
+            ///   * SHA384_Aes256Gcm_RsaPssRsaSha256_X25519
+            ///
+            /// The default value is SHA256_Chacha20Poly1305_EcdsaSecp256r1Sha256_X25519.
+            optional --algorithms algorithms: String
+            /// Path to a certificate that should be served
+            optional --cert certfile: PathBuf
+            /// Path to a private key corresponding to the given certificate
+            optional --key keyfile: PathBuf
+        }
+    }
+}
 
 pub fn main() -> anyhow::Result<()> {
     // Setup tracing.
     tracing_subscriber::fmt::init();
 
-    // Obtain host and port from arguments.
+    let flags = flags::Listen::from_env_or_exit();
+
     let (host, port) = {
-        let mut args = env::args();
+        let host = flags.host.unwrap_or("localhost".to_string());
+        let port = flags.port.unwrap_or(443);
 
-        let _ = args.next().context("Unexpected parameter environment.")?;
-
-        let host = args.next().unwrap_or_else(|| "localhost".to_string());
-        let port = args.next().unwrap_or_else(|| "443".to_string());
-
-        (
-            host,
-            u16::from_str(&port).context("Failed to parse port number.")?,
-        )
+        (host, port)
     };
+
+    let algorithms = flags
+        .algorithms
+        .map(|s| simple_https_client::ciphersuite_from_str(&s).unwrap());
 
     let listener = TcpListener::bind((host.as_str(), port)).unwrap();
 
@@ -37,7 +69,7 @@ pub fn main() -> anyhow::Result<()> {
             .set_read_timeout(Some(d))
             .expect("set_read_timeout call failed");
         println!("New connection established!");
-        match tls13server(&mut stream, &host) {
+        match tls13server(&mut stream, &host, algorithms) {
             Ok(()) => {
                 println!("Connection to {} succeeded\n", host);
             }
