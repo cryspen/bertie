2b6e3df5132798dab2006d8e8c2778c4a633f7de
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-11-20 16:35:19 +0100

    Remove unused dependencies & address clippy warnings

--- simple_https_server/src/lib.rs
+++ simple_https_server/src/lib.rs
@@ -204,12 +204,7 @@ where
     let db = {
         let sni = Bytes::from(host.as_bytes());
 
-        ServerDB(
-            sni,
-            Bytes::from(cert.as_ref()),
-            SignatureKey::from(key.as_ref()),
-            None,
-        )
+        ServerDB(sni, Bytes::from(cert), SignatureKey::from(key), None)
     };
 
     let ent_s = {
